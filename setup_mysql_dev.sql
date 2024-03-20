338a402b785cdaa02ce5397bbea4bd8a7e0b31cd
ayoubzaidane <ayoubcode987@gmail.com>
2024-03-20 00:25:00 +0000

    test sql1

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,17 @@
--- prepares a MySQL server for the project
--- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+-- Create database hbnb_dev_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Create user hbnb_dev@localhost if it doesn't exist
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+
+-- Flush privileges to ensure changes take effect immediately
 FLUSH PRIVILEGES;
+
+-- Informative message
+SELECT CONCAT('Successfully created database hbnb_dev_db and user hbnb_dev@localhost (if it did not exist already).');
