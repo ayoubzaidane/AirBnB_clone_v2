e344fe5ba1841ce5305e15cd82f55226b4ae0a18
ayoubzaidane <ayoubcode987@gmail.com>
2024-04-21 01:34:24 +0100

    README

--- web_flask/0-hello_root.py
+++ web_flask/0-hello_root.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-"""starts a Flask web application"""
+"""
+starts a Flask web applicatio
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index(): 
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
